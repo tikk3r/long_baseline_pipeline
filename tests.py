3c9e3876d56dc6e6a2a938307e69daddff1bde19
Frits Sweijen <fritssweijen@hotmail.com>
2019-10-08 11:45:12 +0200

    Travis integration.

---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+class Test(unittest.TestCase):
+    ''' Test case to see if the scripts run when called as standalone.
+    '''
+    def test_bin():
+        ''' Tests scripts in the bin folder.
+        '''
+        pass
+
+    def test_plugins():
+        ''' Test scripts in the plugins folder.
+        '''
+        pass
+    
